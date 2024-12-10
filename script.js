bb427cfe3a44ad3362c589f9873c3d20c22947dc
Ose <Agbaleose@gmail.com>
2024-12-10 17:47:21 +0100

    Personalised the website, added div bar and animation

---
+++ script.js
@@ -0,0 +1,6 @@
+// Handle form submission for contact
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent successfully!');
+    
+});
